8e2c3d1e2593e947339efe8a6fe245804b8d84a4
leob16 <144756627+leob16@users.noreply.github.com>
2025-08-10 21:16:04 -0300

    Rebuild: correct base and update docs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-Curriculum-vitae/', // nombre EXACTO del repo
+  base:'/mi-Curriculum-vitae/', // nombre EXACTO del repo
 })
